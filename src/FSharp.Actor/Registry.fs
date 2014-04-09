de83ccbcdc8b97a3c1b24de70dc5a84cf8e7e3c7
Colin <colinbul@googlemail.com>
2014-04-09 19:02:19 +0100

    Added rw primtive to registry.

--- src/FSharp.Actor/Registry.fs
+++ src/FSharp.Actor/Registry.fs
@@ -3,19 +3,34 @@
 open System
 
 module Registry = 
+     
+     let private syncObj = new Threading.ReaderWriterLockSlim()
      let actors : Trie.trie<string, IActor> ref = ref Trie.empty
 
      let computeKeysFromPath (path:string) = 
          path.Split([|'/'|], StringSplitOptions.RemoveEmptyEntries) |> List.ofArray
 
-     let resolve address = 
-        Trie.subtrie (computeKeysFromPath address) !actors |> Trie.values
+     let resolve address =
+         try
+            syncObj.EnterReadLock()
+            Trie.subtrie (computeKeysFromPath address) !actors |> Trie.values
+         finally 
+            syncObj.ExitReadLock()
 
      let register (actor:IActor) = 
-         actors := Trie.add (computeKeysFromPath (string actor.Name)) actor !actors
-     
+         try
+            syncObj.EnterWriteLock()
+            actors := Trie.add (computeKeysFromPath actor.Name) actor !actors
+         finally
+            syncObj.ExitWriteLock()
+
      let unregister (actor:IActor) =  
-         actors := Trie.remove (computeKeysFromPath actor.Name) !actors
+         try
+            syncObj.EnterWriteLock()
+            actors := Trie.remove (computeKeysFromPath actor.Name) !actors
+         finally
+            syncObj.ExitWriteLock()
+
 
 [<AutoOpen>]
 module RegistryOperations = 
